532a67667fc7299d4767e72d55029bc42d3b4cde
Neal Lim <55176465+neallithic64@users.noreply.github.com>
2021-08-30 03:36:12 +0800

    COVID: included counts for get case list

--- controllers/cpController.js
+++ controllers/cpController.js
@@ -73,8 +73,9 @@ const cpController = {
 	
 	getCovCaseList: async function(req, res) {
 		try {
-			let cases = await db.findMany(Case, {});
-			res.status(200).send(cases);
+			let cases = await db.findMany(Case, {}),
+				responded = cases.filter(x => x.caseStatus !== "For Review" || x.caseStatus !== "Verifying");
+			res.status(200).send({cases: cases, reportedCount: cases.length, respondedCount: responded.length});
 		} catch (e) {
 			console.log(e);
 			res.status(500).send("Server error.");
