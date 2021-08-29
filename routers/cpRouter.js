c338a57a3705dfadc29e5b8b9afa3868ed8fcb25
Neal Lim <55176465+neallithic64@users.noreply.github.com>
2021-08-30 02:53:36 +0800

    router overhaul tbh
    also added a more specific 404 error route

--- routers/cpRouter.js
+++ routers/cpRouter.js
@@ -9,30 +9,34 @@ router.get("/api", cpController.getHome);
 
 router.get("/api/covid/", cpController.getCovHome);
 router.get("/api/covid/cases", cpController.getCovCaseList);
-router.get("/api/covid/casedeets*", cpController.getCovCaseDetail);
+router.get("/api/covid/casedeets?*", cpController.getCovCaseDetail);
 
 router.get("/api/progp/", cpController.getProHome);
-router.get("/api/progp/getprogs", cpController.getProProgList);
-router.get("/api/progp/getprogs*", cpController.getProFilterProgs);
-router.get("/api/progp/getprogdeets*", cpController.getProProgDetail);
+router.get("/api/progp/progs", cpController.getProProgList);
+router.get("/api/progp/progs?*", cpController.getProFilterProgs);
+router.get("/api/progp/progdeets?*", cpController.getProProgDetail);
 
 
 // POST routes
 router.post("/api/covid/login", cpController.postCovLogin);
-router.post("/api/covid/addpubuser", cpController.postCovAddPublic);
-router.post("/api/covid/addadminuser", cpController.postCovAddAdmin);
+router.post("/api/covid/pubreg", cpController.postCovAddPublic);
+router.post("/api/covid/adminreg", cpController.postCovAddAdmin);
+router.post("/api/covid/caserep", cpController.postCovReportCase);
 
 router.post("/api/progp/login", cpController.postProLogin);
-router.post("/api/progp/adduser", cpController.postProAddUser);
-router.post("/api/progp/addadmin", cpController.postProAddAdmin);
-router.post("/api/progp/addprog", cpController.postProCreateProg);
-router.post("/api/progp/editprog", cpController.postProEditProg);
-router.post("/api/progp/evalprog", cpController.postProEvalProg);
+router.post("/api/progp/userreg", cpController.postProAddUser);
+router.post("/api/progp/adminreg", cpController.postProAddAdmin);
+router.post("/api/progp/progadd", cpController.postProCreateProg);
+router.post("/api/progp/progedit", cpController.postProEditProg);
+router.post("/api/progp/progeval", cpController.postProEvalProg);
 
 
 // error route
+router.get("/api/*", function(req, res) {
+	res.send("404 not found! Not a valid API route.");
+});
 router.get("*", function(req, res) {
-	res.send("404 not found! this is an error");
+	res.send("404 not found! Invalid route.");
 });
 
 module.exports = router;
