b2036d7a5626fa565021175eef167893e00dedba
Neal Lim <55176465+neallithic64@users.noreply.github.com>
2021-09-18 17:41:05 +0800

    COVID: changed symptom model for hardcoded fields

--- models/SymptomModel.js
+++ models/SymptomModel.js
@@ -2,8 +2,17 @@ var mongoose = require('mongoose');
 
 var SymptomSchema = new mongoose.Schema({
 	caseId: Number,
-	sympName: String,
-	sympDays: Number
+	fever: Number,
+	cough: Number,
+	shortbreath: Number,
+	fatigue: Number,
+	bodyache: Number,
+	headache: Number,
+	smell: Number,
+	sorethroat: Number,
+	runnynose: Number,
+	nausea: Number,
+	diarrhea: Number
 });
 
 module.exports = mongoose.model('SYMPTOMS', SymptomSchema);
