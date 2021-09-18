7911e4b4faf9574a736e0cf8279d5e5404b4a0d3
Neal Lim <55176465+neallithic64@users.noreply.github.com>
2021-09-19 01:09:36 +0800

    PROGPLAN: new filtering method, more logging

--- controllers/cpController.js
+++ controllers/cpController.js
@@ -485,10 +485,9 @@ const cpController = {
 				};
 				await db.updateOne(ProgChecklist, filter, update);
 			}
-			let progress = listItems.reduce(function(acc, e) {
-				if (e.checked) acc++;
-			}, 0);
-			console.log(progress);
+			let progress = listItems.filter(e => e.checked);
+			console.table(progress);
+			console.log(progress.length);
 			await db.updateOne(Program, {programId: listItems[0].programId}, {progress: progress});
 			res.status(200).send(listItems[0].programId + " checklist updated!");
 		} catch (e) {
